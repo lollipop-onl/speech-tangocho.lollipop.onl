274d754ac501803e5a538f89d362e88891131091
tamuraryoya <tamuraryoya@team-lab.com>
2020-09-06 21:37:45 +0900

    ⚡️ ログを追加

--- api/gsheets.ts
+++ api/gsheets.ts
@@ -17,42 +17,29 @@ module.exports = async (req: NowRequest, res: NowResponse): Promise<void> => {
       private_key: GOOGLE_PRIVATE_KEY,
     });
 
-    callStats.push(1);
-
     await doc.loadInfo();
-
-    callStats.push(2);
 
     const sheets = [];
 
     for (let i = 0; i < doc.sheetCount - 1; i++) {
       const sheet = doc.sheetsByIndex[i];
 
-      callStats.push(3);
-
       if (sheet.title.startsWith('_')) {
-        callStats.push(4);
-
         continue;
       }
 
       sheets.push(sheet);
-
-      callStats.push(5);
     }
 
-    callStats.push(6);
+    const rows = await Promise.all(sheets.map((sheet) => sheet.getRows()));
 
-    res.json({ message: 'succeed.' , sheets: sheets.length });
+    res.json({ message: 'succeed.' , sheets: rows });
   } catch (error) {
-    callStats.push(7);
-
     console.error(error);
 
     res.json({
       message: 'failed.',
       error,
-      callStats,
     });
   }
 };
