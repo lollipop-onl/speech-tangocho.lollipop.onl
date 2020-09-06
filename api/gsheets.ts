9181a97f43be0f38ca296b4cbffd3bb550c01354
tamuraryoya <tamuraryoya@team-lab.com>
2020-09-06 21:06:23 +0900

    ⚡️ エラーを追跡できるようにした

--- api/gsheets.ts
+++ api/gsheets.ts
@@ -9,33 +9,50 @@ const {
 const doc = new GoogleSpreadsheet(GOOGLE_SPREADSHEET_SHEET_ID);
 
 module.exports = async (req: NowRequest, res: NowResponse): Promise<void> => {
+  const callStats = [];
+
   try {
     await doc.useServiceAccountAuth({
       client_email: GOOGLE_SERVICE_ACCOUNT_EMAIL,
       private_key: GOOGLE_PRIVATE_KEY,
     });
 
+    callStats.push(1);
+
     await doc.loadInfo();
+
+    callStats.push(2);
 
     const sheets = [];
 
     for (let i = 0; i < doc.sheetCount - 1; i++) {
       const sheet = doc.sheetsByIndex[i];
 
+      callStats.push(3);
+
       if (sheet.title.startsWith('_')) {
+        callStats.push(4);
+
         continue;
       }
 
       sheets.push(sheet);
+
+      callStats.push(5);
     }
+
+    callStats.push(6);
 
     res.json({ message: 'succeed.' , sheets });
   } catch (error) {
+    callStats.push(7);
+
     console.error(error);
 
     res.json({
       message: 'failed.',
       error,
+      callStats,
     });
   }
 };
